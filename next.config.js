c92f3960eda07b6562554be90281d5b1c9bfc029
Andrej Novikov <jazzaiman@gmail.com>
2022-02-17 13:50:08 +0200

    feat: project init - ts, next, preact, eslint

---
+++ next.config.js
@@ -0,0 +1,8 @@
+const withPreact = require('next-plugin-preact')
+
+/** @type {import('next').NextConfig} */
+const nextConfig = withPreact({
+  reactStrictMode: true,
+})
+
+module.exports = nextConfig
