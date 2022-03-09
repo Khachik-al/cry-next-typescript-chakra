bd78e0ad293127b011b9da8d1ca8eb8082276373
HP <akhachik@refactory.dev>
2022-03-09 12:12:19 +0400

    fix: spacesize

--- src/Chakra.tsx
+++ src/Chakra.tsx
@@ -1,29 +1,29 @@
 import {
-	ChakraProvider,
-	cookieStorageManager,
-	localStorageManager,
+  ChakraProvider,
+  cookieStorageManager,
+  localStorageManager,
 } from '@chakra-ui/react'
 import { ReactNode } from 'react'
 import { theme } from './styles/theme'
 
 export function Chakra({ cookies, children }: { cookies: string; children: ReactNode }) {
-	const colorModeManager =
-		typeof cookies === 'string'
-			? cookieStorageManager(cookies)
-			: localStorageManager
+  const colorModeManager =
+    typeof cookies === 'string'
+      ? cookieStorageManager(cookies)
+      : localStorageManager
 
-	return (
-		<ChakraProvider colorModeManager={colorModeManager} theme={theme}>
-			{children}
-		</ChakraProvider>
-	)
+  return (
+    <ChakraProvider colorModeManager={colorModeManager} theme={theme}>
+      {children}
+    </ChakraProvider>
+  )
 }
 
 
 export function getServerSideProps({ req }: { req: any }) {
-	return {
-		props: {
-			cookies: req.headers.cookie ?? '',
-		},
-	}
+  return {
+    props: {
+      cookies: req.headers.cookie ?? '',
+    },
+  }
 }
